522310140a94456f3014e8bcc38c82696ea34973
tadeus_gregorian <tade.gregorian@gmail.com>
2017-11-19 14:21:04 +0000

    befor putting Mobile into App Project

--- mobile/src/actions/ui/index.js
+++
@@ -1,8 +0,0 @@
-export const openSideNav = (sideNavCont: SideNav) => ({
-  type: 'OPEN_SIDE_NAV',
-  payload: sideNavCont
-})
-
-export const closeSideNav = () => ({
-  type: 'CLOSE_SIDE_NAV'
-})
--- src/actions/firebaseInit.js
+++ src/actions/firebaseInit.js
@@ -1,12 +1,22 @@
 import firebase from 'firebase'
 
 export const initFirebase = () => {
-  firebase.initializeApp({
-    apiKey: "AIzaSyCHXizDBRTQ_0JpB5-k8FzCRu3UWgCNnxI",
-    authDomain: "plandy-91a56.firebaseapp.com",
-    databaseURL: "https://plandy-91a56.firebaseio.com",
-    projectId: "plandy-91a56",
-  })
+  if(process.env.NODE_ENV === 'production'){
+    firebase.initializeApp({
+      apiKey: "AIzaSyDN42YFUUW6a73BncV17h8pzOKJJUlJO-E",
+      authDomain: "aplano-c4071.firebaseapp.com",
+      databaseURL: "https://aplano-c4071.firebaseio.com",
+      projectId: "aplano-c4071", // project-name: aplano
+    })
+  }else{
+    firebase.initializeApp({
+      apiKey: "AIzaSyCHXizDBRTQ_0JpB5-k8FzCRu3UWgCNnxI",
+      authDomain: "plandy-91a56.firebaseapp.com",
+      databaseURL: "https://plandy-91a56.firebaseio.com",
+      projectId: "plandy-91a56", // project-name: aplano-dev
+    })
+  }
+
   return {type: 'FIREBASE_INITIALIZED'}
 }
 
