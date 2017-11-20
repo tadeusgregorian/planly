62477b0b21531001fe2e8a88c22776eb721c4098
tadeus_gregorian <tade.gregorian@gmail.com>
2017-11-20 12:54:20 +0000

    c

--- src/actions/firebaseInit.js
+++ src/actions/firebaseInit.js
@@ -2,6 +2,7 @@ import firebase from 'firebase'
 
 export const initFirebase = () => {
   if(process.env.REACT_APP_ENV === 'production'){
+    console.log('REACT_APP_ENV: production');
     firebase.initializeApp({
       apiKey: "AIzaSyDN42YFUUW6a73BncV17h8pzOKJJUlJO-E",
       authDomain: "aplano-c4071.firebaseapp.com",
@@ -9,6 +10,7 @@ export const initFirebase = () => {
       projectId: "aplano-c4071", // project-name: aplano
     })
   }else{
+    console.log('REACT_APP_ENV:' +  process.env.REACT_APP_ENV);
     firebase.initializeApp({
       apiKey: "AIzaSyCHXizDBRTQ_0JpB5-k8FzCRu3UWgCNnxI",
       authDomain: "plandy-91a56.firebaseapp.com",
