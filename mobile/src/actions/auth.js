294bdcf752e073aace1c76308a5b645580f5fd78
tadeus_gregorian <tade.gregorian@gmail.com>
2017-11-20 16:51:16 +0000

    deleted Mobile and buildroot

--- mobile/src/actions/auth.js
+++
@@ -1,9 +0,0 @@
-import firebase from 'firebase'
-
-export function signInWithEmailAndPassword (email, pw) {
-	return firebase.auth().signInWithEmailAndPassword(email, pw)
-}
-
-export function logoutFromFirebase () {
-  return firebase.auth().signOut()
-}
