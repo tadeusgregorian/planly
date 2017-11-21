ac948a586da32b33d6c429d178ff0b35e7df0a8b
tadeus_gregorian <tade.gregorian@gmail.com>
2017-11-21 13:08:43 +0000

    hiding admin only functions -> subbar , overtime

--- src/containers/app/roster/weekPlan/shiftBoard/userRow/userCell/index.js
+++ src/containers/app/roster/weekPlan/shiftBoard/userRow/userCell/index.js
@@ -8,6 +8,7 @@ import './styles.css'
 
 type Props =  {
   user: User,
+  currentUser: User,
   position: ?Position,
   weekSum: number,
   weeklyMins: number,
@@ -18,19 +19,21 @@ export default class  extends PureComponent{
   props: Props
 
   render(){
-    const { position, user, weekSum, weeklyMins, timeDetailsVisible } = this.props
+    const { position, user, weekSum, weeklyMins, timeDetailsVisible, currentUser } = this.props
     const color     = position && position.color
     const shortcut  = position && position.shortcut
     const posStyle = { color: color, borderColor: color }
     const sollTime = Math.round((weeklyMins / 60) * 100) / 100
     const negative = weekSum < weeklyMins
     const positive = weekSum > weeklyMins
+    const cUser = currentUser
+    const showTimeDetails = timeDetailsVisible && (cUser.isAdmin || cUser.id === user.id)
 
     return(
       <fb className="userCellMain">
         <fb className='posBox' style={posStyle}>{shortcut}</fb>
         <fb className={cn({userName: 1, big: !timeDetailsVisible})}>{user.name}</fb>
-        <fb className={cn({times: 1, hidden: !timeDetailsVisible})}>
+        <fb className={cn({times: 1, hidden: !showTimeDetails})}>
           <fb className='currentWeek'>
             <fb className={cn({ istTime: 1, negative, positive })}  >{minToTimeString(weekSum)}</fb>
             <fb className='sollTime'>{' / ' + sollTime + ' h'}</fb>
